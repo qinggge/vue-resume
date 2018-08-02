65e10fdd84c18a5af1ad48efe5beba19c62147f9
qinggge <zhuhuang@126.com>
2018-08-03 01:11:59 +0800

    add events & upgrade css

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -14,13 +14,44 @@
             </ol>
         </nav>
         <ol class="panes">
-            <li v-bind:class="{active: currentTab === 0}">tab 1</li>
-            <li v-bind:class="{active: currentTab === 1}">tab 2</li>
-            <li v-bind:class="{active: currentTab === 2}">tab 3</li>
-            <li v-bind:class="{active: currentTab === 3}">tab 4</li>
-            <li v-bind:class="{active: currentTab === 4}">tab 5</li>
-            <li v-bind:class="{active: currentTab === 5}">tab 6</li>
-            <li v-bind:class="{active: currentTab === 6}">tab 7</li>
+            <li v-bind:class="{active: currentTab === 0}">
+                <h2>个人信息</h2>
+                <el-form>
+                    <el-form-item label="姓名">
+                        <el-input v-model="profile.name"></el-input>
+                    </el-form-item>
+                    <el-form-item label="城市">
+                        <el-input v-model="profile.city"></el-input>
+                    </el-form-item>
+                    <el-form-item label="出生年月">
+                        <el-input v-model="profile.birth"></el-input>
+                    </el-form-item>
+                </el-form>
+            </li>
+            <li v-bind:class="{active: currentTab === 1}">
+                <h2>工作经历</h2>
+                <el-form>
+                    <el-button type="primary" v-on:click="addWorkExprience()" class="addExp">添加</el-button>
+                    <div class="wrapper" v-for="(work,index) in workExprience" :key="index">
+                        <el-form-item label="公司">
+                            <el-input v-model="work.company"></el-input>
+                        </el-form-item>
+                        <el-form-item label="工作内容">
+                            <el-input v-model="work.content"></el-input>
+                        </el-form-item>
+                        <i class="el-icon-error" v-on:click="removeWorkExprience(index)"></i>
+                    </div>
+                </el-form>
+            </li>
+            <li v-bind:class="{active: currentTab === 2}">
+                <h2>项目经历</h2>
+            </li>
+            <li v-bind:class="{active: currentTab === 3}">
+                <h2>教育背景</h2>
+            </li>
+            <li v-bind:class="{active: currentTab === 4}">
+                <h2>联系方式</h2>
+            </li>
         </ol>
     </div>
 </template>
@@ -30,8 +61,30 @@ export default {
     data(){
         return {
             currentTab: 0,
-            icons: ['IDcard','centericjob','project','book','tel']
+            icons: ['IDcard','centericjob','project','book','tel'],
+            profile: {
+                name: '',
+                city: '',
+                birth: ''
+            },
+            workExprience: [
+                {company:'',content:''}
+            ]
         }
+    },
+    methods: {
+        addWorkExprience(){
+            this.workExprience.push({
+                company: '',content: ''
+            })
+            console.log(this.workExprience)
+        },
+        removeWorkExprience(index){
+            this.workExprience.splice(index,1)
+        }
+    },
+    created(){
+        
     }
 }
 </script>
@@ -61,10 +114,31 @@ export default {
         }
     }
     > .panes{
+        flex: 1;
         > li{
             display: none;
+            padding: 24px;
             &.active{
                 display: block;
+                height: 100%;
+                overflow: auto;
+                > .el-form{
+                    position: relative;
+                    > .addExp{
+                        position: absolute;
+                        right: 30px;
+                        top: -10px;
+                        z-index: 1;
+                    }
+                }
+            }
+        }
+        .wrapper{
+            position: relative;
+            > i{
+                position: absolute;
+                top: 0;
+                right: 0;
             }
         }
     }
